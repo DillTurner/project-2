c91f5eebeeaa5c734157449dcb85dbdb5ee03791
DillTurner <dillturner678@gmail.com>
2020-02-11 21:03:55 -0800

    created database

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+create database cannabis_db;
+
+
+use cannabis_db;
+
+create Table Janes_data (
+id int auto_increment,
+name varchar (500),
+race varchar (30),
+flavor varchar (500),
+positive_effects varchar (500),
+negative_effects varchar(500),
+medical_usage varchar(500),
+primary key (id)
+);
