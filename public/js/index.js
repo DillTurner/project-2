ce52a7c22752b5e46c58e36f613c704ab08a3263
Ikarus C <56051401+Icortez-UA@users.noreply.github.com>
2020-02-11 23:53:39 -0800

    create index.js
    added sidenav activation script
    added first  call

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    // activate sidenav feature
+    $('.sidenav').sidenav();
+
+
+    //api function call for all strains
+    $.get("/api/all", function(data) {
+    
+    
+        console.log(data);
+      });
+  });
