4f70f783f89a47806b3619213601e4ad33f66433
Ikarus C <56051401+Icortez-UA@users.noreply.github.com>
2020-02-13 12:35:00 -0800

    sync database

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+var db = require("./models");
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
@@ -16,8 +17,8 @@ app.use(express.json());
 require("./routes/api-routes")(app);
 require("./routes/html-routes")(app);
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
