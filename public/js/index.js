ca7d9be4c3f5fa8ff15f14cb75de78c1a59292f9
Ikarus C <56051401+Icortez-UA@users.noreply.github.com>
2020-02-13 12:34:23 -0800

    added test code for data

--- public/js/index.js
+++ public/js/index.js
@@ -5,8 +5,16 @@ $(document).ready(function(){
 
     //api function call for all strains
     $.get("/api/all", function(data) {
-    
+    var photo = data[95].image;
+    var indica= $("#ind");
+
+    var img = $('<img id="pastdynamic_" class= "responsive-img">');
+    img.attr("src", photo);
+    img.appendTo(indica);
     
         console.log(data);
       });
+      $.get("/api/Indica",function(data){
+          console.log(data);
+      })
   });
