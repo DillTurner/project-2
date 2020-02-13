00adfa4405af18afea7b67a0da184f0468cd0db0
Ikarus C <56051401+Icortez-UA@users.noreply.github.com>
2020-02-13 12:34:45 -0800

    updated routes to retrieve test data

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,19 +1,26 @@
 const axios = require('axios');
+var db = require("../models");
 
 module.exports = function(app) {
 
   app.get("/api/all", function(req, res) {
+
+    db.jane.findAll().then(function(data){
+      res.json(data);
+    })
     //request data from external api
-    axios.get('http://strainapi.evanbusse.com/UyDNtOb/strains/search/name/African')
-      .then(function (response) {
-        // handle success
-       res.send(response.data);
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
     
   });
+  app.get("/api/indica",function(req,res){
+    axios.get('http://strainapi.evanbusse.com/UyDNtOb/strains/search/race/Indica')
+    .then(function (response) {
+
+     res.json(response.data);
+    })
+      .catch((error)=>{
+        console.log(error)
+      });
+})
+  
 }
 
