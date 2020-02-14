2c61dde18dcfab5083c99332fa398f186c31b206
Ikarus C <56051401+Icortez-UA@users.noreply.github.com>
2020-02-14 15:39:04 -0800

    added json data

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,25 @@
+
+
 $(document).ready(function(){
     // activate sidenav feature
     $('.sidenav').sidenav();
-
-
+var info= [];
+var title ="";
     //api function call for all strains
-    $.get("/api/all", function(data) {
-    var photo = data[95].image;
-    var indica= $("#ind");
+    $.get("/api/all").then(function(response){
+              $.get("https://strainapi.evanbusse.com/UyDNtOb/strains/search/race/Indica", function(indica) {
+                    
+                   title = indica[0].name;
+                  var infor = response[title];
+                  var IndicaTitle = $('#titleIndica');
+                  $("<h3>"+title+"</h3>").appendTo(IndicaTitle);
+                  console.log(infor);
+                  console.log(indica);
+                });
 
-    var img = $('<img id="pastdynamic_" class= "responsive-img">');
-    img.attr("src", photo);
-    img.appendTo(indica);
-    
+      });
+      $.get("/api/hybrid", function(data) {
         console.log(data);
       });
-      $.get("/api/Indica",function(data){
-          console.log(data);
-      })
+
   });
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,18 +1,51 @@
 const axios = require('axios');
 var db = require("../models");
-
+const jsonAll = require("../all-strains.json")
 module.exports = function(app) {
 
   app.get("/api/all", function(req, res) {
 
-    db.jane.findAll().then(function(data){
-      res.json(data);
-    })
+ 
+      res.json(jsonAll);
+  
     //request data from external api
     
   });
+  // call for all strains
+  /*app.get("/api/all",function(req,res){
+    axios.get('http://strainapi.evanbusse.com/UyDNtOb/strains/search/all')
+    .then(function (response) {
+    
+     res.json(response.data);
+    })
+      .catch((error)=>{
+        console.log(error)
+      });
+})*/
+
   app.get("/api/indica",function(req,res){
-    axios.get('http://strainapi.evanbusse.com/UyDNtOb/strains/search/race/Indica')
+    axios.get('https://strainapi.evanbusse.com/UyDNtOb/strains/search/race/Indica')
+    .then(function (response) {
+      console.log(response);
+     res.json(response);
+    })
+      .catch((error)=>{
+        console.log(error)
+      });
+    })
+ /* app.get("/api/sativa",function(req,res){
+    axios.get('strainapi.evanbusse.com/UyDNtOb/strains/search/race/Sativa')
+    .then(function (response) {
+
+     res.json(response.data);
+    })
+      .catch((error)=>{
+        console.log(error)
+      });
+    })
+    
+  app.get("/api/hybrid",function(req,res){
+    axios.get('strainapi.evanbusse.com/UyDNtOb/strains/search/race/Hybrid')
     .then(function (response) {
 
      res.json(response.data);
