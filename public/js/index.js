14fd4a50acba83b937e47ef499de27070d02267d
Kory Kao <korykao@gmail.com>
2020-02-13 21:14:54 -0800

    test

--- public/js/index.js
+++ public/js/index.js
@@ -6,6 +6,9 @@ $(document).ready(function(){
     //api function call for all strains
     $.get("https://strainapi.evanbusse.com/UyDNtOb/strains/search/race/Indica", function(data) {
         
+      var title = data[0].name
+      title.appendTo($('#titleIndica'));
+
         console.log(data);
       });
       $.get("/api/all", function(data) {
