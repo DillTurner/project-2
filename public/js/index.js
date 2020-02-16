4d969f467fafefe028a0b1e31a9ac608bb678878
Ikarus C <56051401+Icortez-UA@users.noreply.github.com>
2020-02-15 18:08:38 -0800

    updates routes to send json files

--- public/js/index.js
+++ public/js/index.js
@@ -3,23 +3,20 @@
 $(document).ready(function(){
     // activate sidenav feature
     $('.sidenav').sidenav();
-var info= [];
-var title ="";
     //api function call for all strains
     $.get("/api/all").then(function(response){
               $.get("https://strainapi.evanbusse.com/UyDNtOb/strains/search/race/Indica", function(indica) {
                     
                    title = indica[0].name;
-                  var infor = response[title];
                   var IndicaTitle = $('#titleIndica');
                   $("<h3>"+title+"</h3>").appendTo(IndicaTitle);
-                  console.log(infor);
-                  console.log(indica);
+                  console.log(response);
+                  //console.log(indica);
                 });
 
       });
       $.get("/api/hybrid", function(data) {
-        console.log(data);
+        //console.log(data);
       });
 
   });
