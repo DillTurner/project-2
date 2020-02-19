a4323e78387f7e46958debee2ba070748b2effa0
Kory Kao <korykao@gmail.com>
2020-02-18 20:10:01 -0800

    indica page

--- public/js/indica.js
+++ public/js/indica.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
+    $('.modal').modal();
 
 $.get("/api/indica", function(response) {
 
     var RandomStrain = response[Math.floor(Math.random()*response.length)];
+    var RandomStrain3 = response[Math.floor(Math.random()*response.length)];
+    var RandomStrain4 = response[Math.floor(Math.random()*response.length)];
+    var RandomStrain5 = response[Math.floor(Math.random()*response.length)];
 
-    
+// Card 1
     var IndicaTitle = $('#titleIndica');
-    $("<h3>"+RandomStrain.name+"</h3>").appendTo(IndicaTitle);
+    $("<h2>"+RandomStrain.name+"</h2>").appendTo(IndicaTitle);
 
     var IndicaPositive = $('#positiveIndica');
     $("<h5>"+RandomStrain.Value_effects_positive+"</h5>").appendTo(IndicaPositive);
@@ -19,9 +23,26 @@ $.get("/api/indica", function(response) {
     $("<h5>"+RandomStrain.Value_flavors+"</h5>").appendTo(IndicaFlavors);
 
     var IndicaMedical = $('#medicalIndica');
-    $("<h5>"+rRandomStrain.Value_effects_medical+"</h5>").appendTo(IndicaMedical);
-
+    $("<h5>"+RandomStrain.Value_effects_medical+"</h5>").appendTo(IndicaMedical);
     
+// Card 2
+    var RandomStrain2 = response[Math.floor(Math.random()*response.length)];
+
+    var IndicaTitle2 = $('#titleIndica2');
+    $("<h5>"+RandomStrain2.name+"</h5>").appendTo(IndicaTitle2);
+
+    var IndicaPositive2 = $('#positiveIndica2');
+    $("<h7>"+RandomStrain2.Value_effects_positive+"</h7>").appendTo(IndicaPositive2);
+
+    var IndicaNegative2 = $('#negativeIndica2');
+    $("<h7>"+RandomStrain2.Value_effects_negative+"</h7>").appendTo(IndicaNegative2);
+
+    var IndicaFlavors2 = $('#flavorsIndica2');
+    $("<h7>"+RandomStrain2.Value_flavors+"</h7>").appendTo(IndicaFlavors2);
+
+    var IndicaMedical2 = $('#medicalIndica2');
+    $("<h7>"+rRandomStrain2.Value_effects_medical+"</h7>").appendTo(IndicaMedical2);
+
       console.log(response);
 
     });
