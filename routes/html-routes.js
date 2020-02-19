eb43059c7f3565e19ed5da66f3948d68c627227b
Ikarus C <56051401+Icortez-UA@users.noreply.github.com>
2020-02-18 18:54:12 -0800

    update

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -26,5 +26,13 @@ module.exports = function(app) {
   app.get('/login', function(request, response) {
     response.sendFile(path.join(__dirname + '../public/login.html'));
   });
+  app.get('/profile', function(request, response) {
+    /*if (request.session.loggedin) {
+      response.send('Welcome back, ' + request.session.username + '!');
+    } else {
+      response.send('Please login to view this page!');
+    }
+    response.end();*/
+  });
 // need to add a profile html route.
 };
