0b2c45096225d919a418963eceb79c17bc75c3a5
Kory Kao <korykao@gmail.com>
2020-02-18 21:25:19 -0800

    all page

--- public/js/all.js
+++ public/js/all.js
@@ -6,12 +6,34 @@ $(document).ready(function(){
 
 $.get("/api/all", function(response) {
 
-for (var i = 0; i < respones.length; i++) {
+for (var i = 0; i < 500; i++) {
 
-    var Names = $('#allStrains');
+    var Names = $('#column1');
 $("<p>"+response[i].name+"</p>").appendTo(Names);
 
-    console.log(response);
-}});
+}
+for (var i = 501; i < 1000; i++) {
+
+    var Names2 = $('#column2');
+$("<p>"+response[i].name+"</p>").appendTo(Names2);
+
+}
+
+for (var i = 1001; i < 1500; i++) {
+
+    var Names3 = $('#column3');
+$("<p>"+response[i].name+"</p>").appendTo(Names3);
+
+}
+
+for (var i = 1501; i < 1971; i++) {
+
+    var Names4 = $('#column4');
+$("<p>"+response[i].name+"</p>").appendTo(Names4);
+
+}
+
+console.log(response);
+});
 
 });
