bcc87f75b8a2752b0fb40b97d6abefed19c004eb
Kory Kao <korykao@gmail.com>
2020-02-18 21:34:12 -0800

    all links

--- public/js/all.js
+++ public/js/all.js
@@ -12,24 +12,24 @@ for (var i = 0; i < 500; i++) {
 $("<p>"+response[i].name+"</p>").appendTo(Names);
 
 }
-for (var i = 501; i < 1000; i++) {
+for (var j = 501; j < 1000; j++) {
 
     var Names2 = $('#column2');
-$("<p>"+response[i].name+"</p>").appendTo(Names2);
+$("<p>"+response[j].name+"</p>").appendTo(Names2);
 
 }
 
-for (var i = 1001; i < 1500; i++) {
+for (var k = 1001; k < 1500; k++) {
 
     var Names3 = $('#column3');
-$("<p>"+response[i].name+"</p>").appendTo(Names3);
+$("<p>"+response[k].name+"</p>").appendTo(Names3);
 
 }
 
-for (var i = 1501; i < 1971; i++) {
+for (var l = 1501; l < 1971; l++) {
 
     var Names4 = $('#column4');
-$("<p>"+response[i].name+"</p>").appendTo(Names4);
+$("<p>"+response[l].name+"</p>").appendTo(Names4);
 
 }
 
